2d65ae128363b4ba70834e3d9625a7543fe5815f
Julian Nordin <julian.nordin94@gmail.com>
2025-08-06 20:23:17 +0200

    feat: add seed data for Books/Members (DbInitializer), auto-migrate on startup

--- LibrarySystem.Api/Program.cs
+++ LibrarySystem.Api/Program.cs
@@ -14,6 +14,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+    DbInitializer.Initialize(db);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
