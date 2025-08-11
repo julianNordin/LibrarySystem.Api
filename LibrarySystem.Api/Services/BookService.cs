04ab424427cc89fd223e2d6c951f8d6f021ff4a3
Julian Nordin <julian.nordin94@gmail.com>
2025-08-11 18:47:29 +0200

    feat: implement BookService GetAll/GetById/Create

--- LibrarySystem.Api/Services/BookService.cs
+++ LibrarySystem.Api/Services/BookService.cs
@@ -1,5 +1,6 @@
 using LibrarySystem.Api.Data;
 using LibrarySystem.Api.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibrarySystem.Api.Services;
 
@@ -12,11 +13,22 @@ public class BookService : IBookService
         _context = context;
     }
 
-    public Task<IEnumerable<Book>> GetAllAsync() => throw new NotImplementedException();
+    public async Task<IEnumerable<Book>> GetAllAsync()
+    {
+        return await _context.Books.AsNoTracking().ToListAsync();
+    }
 
-    public Task<Book?> GetByIdAsync(int id) => throw new NotImplementedException();
+    public async Task<Book?> GetByIdAsync(int id)
+    {
+        return await _context.Books.AsNoTracking().FirstOrDefaultAsync(b => b.Id == id);
+    }
 
-    public Task<Book> CreateAsync(Book book) => throw new NotImplementedException();
+    public async Task<Book> CreateAsync(Book book)
+    {
+        _context.Books.Add(book);
+        await _context.SaveChangesAsync();
+        return book;
+    }
 
     public Task<bool> UpdateAsync(int id, Book book) => throw new NotImplementedException();
 
